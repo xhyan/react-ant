c8bffafda496cca27799e2ed67c31e46ba167a3d
yanliwei <imissyou.xhyan@gmail.com>
2016-08-04 23:22:57 +0800

    新增table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import Todos from './Todos/Todos';
+import Tabler from './Table/Table';
 import MainLayout from '../layouts/MainLayout/MainLayout';
 
 const App = ({ location }) => {
   return (
     <MainLayout>
       <Todos location={location} />
+      <Tabler />
     </MainLayout>
   );
 };
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import { Table, Icon } from 'antd';
+
+class Tabler extends Component{
+
+  loadData(){
+    this.setState({columns : [{
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    }]});
+
+    this.setState({dataSource :  [{
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号'
+    }, {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号'
+    }]});
+  }
+
+  componentDidMount(){
+    this.loadData();
+  }
+
+  render(){
+    const {columns, dataSource} = this.state;
+    return <Table dataSource={dataSource} columns={columns} />
+
+  };
+}
+
+export default Tabler;
