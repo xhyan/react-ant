e23b9a3f5dbe806037aaafca31159e2019ab234f
yanliwei <imissyou.xhyan@gmail.com>
2016-08-05 23:15:02 +0800

    修复问题

--- src/entries/index.js
+++ src/entries/index.js
@@ -1,13 +1,13 @@
 import './index.html';
 import './index.less';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import React from 'react';
 import { browserHistory } from 'react-router';
 import App from '../components/App';
 import Routes from '../routes/index';
-import Counter from '../containers/App'
-import configureStore from '../store/configStore'
+import Counter from '../containers/App';
+import configureStore from '../store/configStore';
 // ReactDOM.render(<Routes history={browserHistory} />, document.getElementById('root'));
 
 const store = configureStore()
--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/index';
+import reducer from '../reducers';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 export default function configureStore(initialState){
-  return createStore(createStoreWithMiddleware(rootReducer, initialState));
+  return createStoreWithMiddleware(reducer, initialState);
 }
